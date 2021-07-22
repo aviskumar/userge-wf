571ce2615ccfa18eee7e1956d36c319034e0311c
aviskumar <85635883+aviskumar@users.noreply.github.com>
2021-07-22 11:36:11 +0530

    Create action.sh

---
+++ action.sh
@@ -0,0 +1,5 @@
+git clone -b master https://github.com/UsergeTeam/Userge.git /app/
+cd Userge
+mv config.env.sample config.env
+docker build . -t userge
+docker run -d --restart on-failure --name userge_1 userge

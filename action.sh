b45e910e04a7b42b32a40cbc100fdcb68ed2c07d
aviskumar <85635883+aviskumar@users.noreply.github.com>
2021-07-22 19:29:59 +0530

    Delete action.sh

--- action.sh
+++
@@ -1,5 +0,0 @@
-git clone -b master https://github.com/UsergeTeam/Userge.git
-cd Userge
-mv config.env.sample config.env
-docker build . -t userge
-docker run -d --restart on-failure --name userge_1 userge
